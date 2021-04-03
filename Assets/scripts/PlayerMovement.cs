21889e7a7278a159693bcaa76037158408faca60
Alexandre-Toncelli <alexandre.toncelli@gmail.com>
2021-04-03 20:22:38 +0200

    1.0

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpForce;
+    public float moveSpeed;
+
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform GroundCheck_L;
+    public Transform GroundCheck_R;
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapArea(GroundCheck_L.position, GroundCheck_R.position);
+
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+
+        MovePlayer(horizontalMovement);
+
+        Flip(rb.velocity.x);
+
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed", characterVelocity);
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+
+    void Flip(float _velocity)
+    {
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        } else if (_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+}
